a5fd22e4234326d93d2422feaa1298616577eda4
elprup <elprup@126.com>
2012-01-07 13:54:19 +0800

    filter added

--- mlogging/mlogging.py
+++ mlogging/mlogging.py
@@ -1,11 +1,11 @@
-'''papaya logging module'''
+'''mlogging module'''
 import os
 import logging
 
-_default_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+_default_formatter = logging.Formatter('%(asctime)s %(module)s %(name)s %(levelname)s %(message)s')
 _default_local_root = '/tmp'
 
-def config(name='default', output=['screen'], level=['all'],local_root=_default_local_root):
+def config(name='default', outputs=['screen'], levels=['all'], local_root=_default_local_root):
     '''Config a logger
 
     output: a list of output types, they can be
@@ -16,12 +16,37 @@ def config(name='default', output=['screen'], level=['all'],local_root=_default_
         all, debug, info, warning, error, critical
     '''
     logger = logging.getLogger(name)
-    for op in output:
+    # default record all level info
+    logger.setLevel(logging.DEBUG)
+    for op in outputs:
         if op == 'screen':
             logger.addHandler(_screen_handler)
         elif op == 'local':
             logger.addHandler(get_local_handler(name, local_root))
+    levelmap = {'debug':logging.DEBUG,
+                'info':logging.INFO,
+                'warning':logging.WARNING,
+                'error':logging.ERROR,
+                'critical':logging.CRITICAL}
+    logging_level = []
+    for lvl in levels:
+        if lvl == 'all':
+            logging_level.extend(levelmap.values())
+            break
+        log_lvl = levelmap.get(lvl,None)
+        if log_lvl:
+            logging_level.append(log_lvl)
+    logging_level = set(logging_level)
+    logger.addFilter(MultipleFilter(logging_level))
     return logger
+
+class MultipleFilter(logging.Filter):
+    '''Choose log which level in logging_level'''
+    def __init__(self, logging_level):
+        self.logging_level = logging_level
+        logging.Filter.__init__(self)
+    def filter(self, record):
+        return record.levelno in self.logging_level
 
 def get_screen_handler():
     '''Return a screen appender of loggging'''
@@ -35,16 +60,18 @@ def get_local_handler(name,local_root):
     log_file =  os.path.join(local_root, name.replace('.','/'))
     log_dir = os.path.dirname(log_file)
     if not os.path.exists(log_dir):
-        os.makedir(log_dir)
+        os.makedirs(log_dir)
     handler = logging.FileHandler( log_file, 'a')
     handler.setFormatter(_default_formatter)
     return handler
 
 def get_null_handler():
+    ''' Dummy handler to avoid no handler warning'''
     handler = logging.FileHandler('/dev/null','w')
     return handler
 
 def reset(name):
+    '''Reset log to forbid all handler output and no handler warning'''
     log = logging.getLogger(name)
     for h in log.handlers:
         log.removeHandler(h)
--- mlogging/test.py
+++ mlogging/test.py
@@ -3,53 +3,71 @@ import unittest
 import sys
 from StringIO import StringIO
 import subprocess
-# test case require
+import random
+import shutil
+# target module to test
 import mlogging
 
+#util
+def gen_random_string(length=10):
+    return ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for x in range(10))
+def gen_temp_dir(root='/tmp'):
+    '''generate a directory which doesn't exist before'''
+    while(True):
+        temp_dir = os.path.join(root, gen_random_string())
+        if not os.path.exists(temp_dir):
+            break
+    return temp_dir
+
 class TestMLogging(unittest.TestCase):
-    ''' Test ppylogging'''
+    ''' Test mlogging'''
 
     def test_screen_logging(self):
         # make sure logging to screen works
-        name = 'screentest'
-        log = mlogging.config(name=name, output=['screen'])
+        name = 'screentest1'
+        log = mlogging.config(name=name, outputs=['screen'], levels=['warning','debug'])
         # redirect stream to string io to capture
         log.handlers[0].stream = StringIO()
         log.warning('warning')
+        log.info('info')
+        log.debug('debug')
         log_output = log.handlers[0].stream.getvalue()
-        msg = log_output.split()
-        self.assertEqual(msg[2:5], [name,'WARNING','warning'])
+        msg_warning = log_output.strip().split('\n')[0].split()
+        self.assertEqual(msg_warning[3:6], [name,'WARNING','warning'])
+        msg_debug = log_output.strip().split('\n')[1].split()
+        self.assertEqual(msg_debug[3:6], [name,'DEBUG','debug'])
         mlogging.reset(name)
+        
  
     def test_local_logging(self):
         # logging to local file test
-        local_root = '/tmp'
+        local_root = gen_temp_dir()
         name = 'local.test.test'
-        log = mlogging.config(name=name, output=['local'], local_root=local_root)
+        log = mlogging.config(name=name, outputs=['local'], local_root=local_root)
         log.warning('warning')
         log_file = os.path.join(local_root, name.replace('.','/'))
         tail = subprocess.Popen(["tail",log_file], stdout=subprocess.PIPE)
         msg = tail.stdout.read().strip().split('\n')[-1].split()
-        self.assertEqual(msg[2:5], [name,'WARNING','warning'])
-        os.remove(log_file)
+        self.assertEqual(msg[3:6], [name,'WARNING','warning'])
+        shutil.rmtree(local_root)
         mlogging.reset(name)
- 
+        
     def test_combine_logging(self):
         # test combined logging
-        local_root = '/tmp'
+        local_root = gen_temp_dir()
         name = 'local.test.test'
-        log = mlogging.config(name=name, output=['screen','local'], local_root=local_root)       
+        log = mlogging.config(name=name, outputs=['screen','local'], local_root=local_root)       
         # redirect stream to string io to capture
         log.handlers[0].stream = StringIO()
         log.warning('warning')
         log_output = log.handlers[0].stream.getvalue()
         screen_msg = log_output.split()
-        self.assertEqual(screen_msg[2:5], [name,'WARNING','warning'])
+        self.assertEqual(screen_msg[3:6], [name,'WARNING','warning'])
         log_file = os.path.join(local_root, name.replace('.','/'))
         tail = subprocess.Popen(["tail",log_file], stdout=subprocess.PIPE)
         msg_local = tail.stdout.read().strip().split('\n')[-1].split()
-        self.assertEqual(msg_local[2:5], [name,'WARNING','warning'])
-        os.remove(log_file)
+        self.assertEqual(msg_local[3:6], [name,'WARNING','warning'])
+        shutil.rmtree(local_root)
         mlogging.reset(name)
 
 if __name__ == '__main__':
